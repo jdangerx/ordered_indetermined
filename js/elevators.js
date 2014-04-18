50eb11640944358277078d295c3707d0b3af2cdc
John Xia <john.danger.xia@gmail.com>
2014-04-18 15:08:27 -0500

    moving lines!

--- js/elevators.js
+++ js/elevators.js
@@ -2,13 +2,12 @@
 var camera, renderer;
 var scene = new THREE.Scene();
 var people = [];
-var DX = 30;
+var dS = 70;
 
 function init() {
-  scene.fog = new THREE.Fog(0x111111);
+  scene.fog = new THREE.Fog(0x111111, 1, 500);
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   camera.position.z = 200;
-  // geometry = new THREE.Geometry();
 
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(0x111111, 1);
@@ -16,61 +15,79 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  var person = new Person();
-  people.push(person);
-  // for (var i = 0; i < 10; i++) {
-  //   var vertex = new THREE.Vector3(i, 0, 0);
-  //   geometry.vertices.push(vertex);
-  // }
-  // material = new THREE.LineBasicMaterial({color: 0xFF2233, linewidth: 10, fog:true});
-  // polyline = new THREE.Line(geometry, material);
-  // scene.add(polyline);
+  // var person = new Person();
+  for (var i = 0; i < 100; i++) {
+    people.push(new Person());
+  }
   render();
 }
 
 function Person() {
   var geometry = new THREE.Geometry();
-  var taillen = 10;
+  var taillen = 30;
+  var init_pos = new THREE.Vector3(Math.round(Math.random()*2*dS-dS),
+                                   Math.round(Math.random()*2*dS-dS),
+                                   Math.round(Math.random()*2*dS-dS));
   for (var i = 0; i < taillen; i++) {
     var vertex = new THREE.Vector3(i, 0, 0);
+    vertex.add(init_pos);
     geometry.vertices.push(vertex);
   }
-  var material = new THREE.LineBasicMaterial({color: 0xFF2233,
-                                              linewidth: 10,
+  var color = new THREE.Color(Math.random()*0.5+0.5,
+                              Math.random()*0.5+0.5,
+                              Math.random()*0.5+0.5);
+  var material = new THREE.LineBasicMaterial({color: color.getHex(),
+                                              linewidth: 2,
                                               fog:true});
   this.polyline = new THREE.Line(geometry, material);
   scene.add(this.polyline);
   this.velocity = new THREE.Vector3(1, 0, 0);
   this.sign = 1;
+  this.dests = [];
 }
 
 Person.prototype.move = function () {
+  if (this.dests.length == 0) {
+    this.dests.push(new THREE.Vector3(Math.round(Math.random()*2*dS-dS),
+                                      Math.round(Math.random()*2*dS-dS),
+                                      Math.round(Math.random()*2*dS-dS)));
+  }
+
   var vertices = this.polyline.geometry.vertices
   for (var i = 0, len = vertices.length; i < len; i++) {
     if (i < len - 1) {
-      var ith = vertices[i];
-      var iplusone = vertices[i+1];
-      ith.copy(iplusone);
+      vertices[i].copy(vertices[i+1]);
     } else {
       var last_vertex = vertices[i];
-      if (Math.abs(last_vertex.x) < 100) {
-        this.velocity.setX(Math.random()*5);
-        last_vertex.add(this.velocity.multiplyScalar(this.sign));
+      var dX = clamp(this.dests[0].x - last_vertex.x, -1, 1);
+      var dY = clamp(this.dests[0].y - last_vertex.y, -1, 1);
+      var dZ = clamp(this.dests[0].z - last_vertex.z, -1, 1);
+      if (dX != 0) {
+        this.velocity.set(dX, 0, 0);
+      } else if (dY != 0) {
+        this.velocity.set(0, dY, 0);
+      } else if (dZ != 0) {
+        this.velocity.set(0, 0, dZ);
       } else {
-        this.sign = this.sign * -1;
-        last_vertex.add(this.velocity.multiplyScalar(this.sign));
+        this.velocity.set(0, 0, 0);
+        this.dests.pop();
       }
+      last_vertex.add(this.velocity);
     }
   }
   this.polyline.geometry.verticesNeedUpdate = true;
 }
 
+function clamp(x, a, b) {
+  return Math.max(a, Math.min(x, b));
+}
+
 function render() {
-  // polyline.rotation.x += 0.01;
-  // polyline.rotation.y += 0.01;
-  // polyline.rotation.z += 0.01;
   for (var i in people) {
     people[i].move();
+    people[i].polyline.rotation.y += 0.003;
+    // people[i].polyline.rotation.x = 0.5;
+    // people[i].polyline.rotation.z += 0.01;
   }
 
 
